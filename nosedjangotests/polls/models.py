5b647ea4affb64131f341e7f447b04ef0248eba8
winhamwr <winhamwr@gmail.com>
2015-02-04 18:49:23 -0500

    Refs #33. Attempt at fixture-failure bleed through

--- nosedjangotests/polls/models.py
+++ nosedjangotests/polls/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
+
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
 
 class Choice(models.Model):
     content_type = models.ForeignKey(ContentType)
@@ -13,3 +15,14 @@ class Choice(models.Model):
 
     choice = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class PollPair(models.Model):
+    first_poll = models.ForeignKey(
+        Poll,
+        related_name='first_pairs',
+    )
+    last_poll = models.ForeignKey(
+        Poll,
+        related_name='last_pairs',
+    )
--- nosedjangotests/polls/tests/test_fixture_bleedthrough.py
+++ nosedjangotests/polls/tests/test_fixture_bleedthrough.py
@@ -1,9 +1,15 @@
 from django.test import TestCase, TransactionTestCase
 
+from nosedjangotests.polls.models import Poll, PollPair
 from nosedjangotests.polls.tests.test1 import (
     _test_fixtures_1,
     _test_fixtures_2,
 )
+
+
+def _assert_no_data(self):
+    self.assertEqual(Poll.objects.count(), 0)
+    self.assertEqual(PollPair.objects.count(), 0)
 
 
 class AAATestCase(TransactionTestCase):
@@ -22,3 +28,21 @@ class BBBTestCase(TestCase):
 
     def test(self):
         _test_fixtures_1(self)
+
+
+class CCCBrokenFixtureTestCase(TestCase):
+    fixtures = [
+        'poll_pairs',
+    ]
+
+    def test(self):
+        pp = PollPair.objects.get(pk=1)
+        assert pp.first_poll
+        _assert_no_data(self)
+
+
+class DDDAfterBrokenFixtureNoLeftoversTestCase(TestCase):
+    fixtures = []
+
+    def test(self):
+        _assert_no_data(self)
