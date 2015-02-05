464f439f921c8598572844899e30049e42b41c62
winhamwr <winhamwr@gmail.com>
2015-02-04 20:53:13 -0500

    Refs #33. Only need one fixture to PoC

--- nosedjangotests/polls/tests/test_fixture_bleedthrough.py
+++ nosedjangotests/polls/tests/test_fixture_bleedthrough.py
@@ -1,6 +1,6 @@
 from django.test.testcases import TestCase
 
-from nosedjangotests.polls.models import Choice, Poll
+from nosedjangotests.polls.models import Choice
 
 
 class _SubclassedTestCase(TestCase):
@@ -10,15 +10,12 @@ class _SubclassedTestCase(TestCase):
 class AAAFixtureTestCase(_SubclassedTestCase):
     fixtures = [
         'choices',
-        'polls1',
     ]
 
     def test_to_load_fixtures(self):
-        self.assertEqual(Poll.objects.count(), 1)
         self.assertEqual(Choice.objects.count(), 1)
 
 
 class BBBNoLeftoverFixturesTestCase(TestCase):
     def test_no_data_left_over_from_previous_fixture(self):
-        self.assertEqual(Poll.objects.count(), 0)
         self.assertEqual(Choice.objects.count(), 0)
