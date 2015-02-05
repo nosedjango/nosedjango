aa0dc4bf2125b6f82879db9e0211fe6b627c055f
Jason Ward <jason.ward@policystat.com>
2015-02-05 11:50:33 -0500

    refs #33: Added a test showing it's only bleed through with fixtures.

--- nosedjangotests/polls/tests/test_fixture_bleedthrough.py
+++ nosedjangotests/polls/tests/test_fixture_bleedthrough.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from django.test.testcases import TestCase
 
-from nosedjangotests.polls.models import Choice
+from nosedjangotests.polls.models import Choice, Poll
 
 
 class _SubclassedTestCase(TestCase):
@@ -19,3 +21,16 @@ class AAAFixtureTestCase(_SubclassedTestCase):
 class BBBNoLeftoverFixturesTestCase(TestCase):
     def test_no_data_left_over_from_previous_fixture(self):
         self.assertEqual(Choice.objects.count(), 0)
+
+
+class AAANonFixtureTestCase(_SubclassedTestCase):
+    def test_to_load_fixtures(self):
+        Poll.objects.create(
+            pub_date=datetime.now()
+        )
+        self.assertEqual(Poll.objects.count(), 1)
+
+
+class BBBNoLeftoverNonFixturesTestCase(TestCase):
+    def test_no_data_left_over_from_previous_fixture(self):
+        self.assertEqual(Poll.objects.count(), 0)
