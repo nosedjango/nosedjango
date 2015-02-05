9d5cb0e9d9da0a939d44f5e54b1935deacb5770c
Jason Ward <jason.ward@policystat.com>
2015-02-05 12:28:09 -0500

    refs #33: Updated the tests.

--- nosedjangotests/polls/tests/test_fixture_bleedthrough.py
+++ nosedjangotests/polls/tests/test_fixture_bleedthrough.py
@@ -1,15 +1,9 @@
-from datetime import datetime
-
 from django.test.testcases import TestCase
 
-from nosedjangotests.polls.models import Choice, Poll
+from nosedjangotests.polls.models import Choice
 
 
-class _SubclassedTestCase(TestCase):
-    pass
-
-
-class AAAFixtureTestCase(_SubclassedTestCase):
+class AAAFixtureTestCase(TestCase):
     fixtures = [
         'choices',
     ]
@@ -18,19 +12,6 @@ class AAAFixtureTestCase(_SubclassedTestCase):
         self.assertEqual(Choice.objects.count(), 1)
 
 
-class BBBNoLeftoverFixturesTestCase(TestCase):
+class BBBNoFixturesFollowingWithFixturesTestCase(TestCase):
     def test_no_data_left_over_from_previous_fixture(self):
         self.assertEqual(Choice.objects.count(), 0)
-
-
-class AAANonFixtureTestCase(_SubclassedTestCase):
-    def test_to_load_fixtures(self):
-        Poll.objects.create(
-            pub_date=datetime.now()
-        )
-        self.assertEqual(Poll.objects.count(), 1)
-
-
-class BBBNoLeftoverNonFixturesTestCase(TestCase):
-    def test_no_data_left_over_from_previous_fixture(self):
-        self.assertEqual(Poll.objects.count(), 0)
