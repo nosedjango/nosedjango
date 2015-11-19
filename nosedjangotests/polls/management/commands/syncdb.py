de3d5f3a2b14b8b2bb692da3fa53f2d40dc80d6c
Jason Ward <jason.ward@policystat.com>
2015-11-19 16:31:47 -0500

    refs #41: Fixed help text.

--- nosedjangotests/polls/management/commands/syncdb.py
+++ nosedjangotests/polls/management/commands/syncdb.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Do nothing, other than shadow djangos syncdb command.'
+    help = "Do nothing, other than shadow django's syncdb command."
 
     def handle(self, *args, **options):
         pass
