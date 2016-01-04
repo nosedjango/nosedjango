d90daa006cf69525bfeedf8dc7cd92274fc48f15
Jason Ward <jason.ward@policystat.com>
2016-01-04 10:31:25 -0500

    refs #42: Working a theory.

--- nosedjangotests/polls/models.py
+++ nosedjangotests/polls/models.py
@@ -3,14 +3,14 @@ from django.contrib.contenttypes import generic
 
 
 class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question = models.CharField(max_length=200, db_index=True)
+    pub_date = models.DateTimeField('date published', db_index=True)
 
 
 class Choice(models.Model):
-    content_type = models.ForeignKey('contenttypes.ContentType')
-    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey('contenttypes.ContentType', db_index=True)
+    object_id = models.PositiveIntegerField(db_index=True)
     poll = generic.GenericForeignKey('content_type', 'object_id')
 
-    choice = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice = models.CharField(max_length=200, db_index=True)
+    votes = models.IntegerField(default=0, db_index=True)
