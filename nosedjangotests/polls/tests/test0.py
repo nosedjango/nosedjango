6cd92e074ab415335f1bb4843eccbd9f00c0b010
Jason Ward <jason.ward@policystat.com>
2016-01-06 14:55:06 -0500

    refs #44: Added failing tests.

--- nosedjangotests/polls/tests/test0.py
+++ nosedjangotests/polls/tests/test0.py
@@ -1,7 +1,14 @@
+from contextlib import contextmanager
+from datetime import datetime
 
+try:
+    from django.db.transaction import atomic
+except ImportError:
+    from django.db.transaction import commit_on_success as atomic
+from django.db import transaction
 from django.test import TestCase
 
-from nosedjangotests.polls.models import Choice
+from nosedjangotests.polls.models import Choice, Poll
 
 
 def _test_using_content_types(self):
@@ -23,3 +30,50 @@ class ContentTypeTestCase(TestCase):
 
 class CherryPyTestCase(ContentTypeTestCase):
     start_live_server = True
+
+
+@contextmanager
+def set_autocommit(value):
+    old_value = transaction.get_autocommit()
+    transaction.set_autocommit(value)
+    yield
+    transaction.set_autocommit(old_value)
+
+
+class FixtureBleedThroughTestCase(TestCase):
+    fixtures = [
+        'polls1.json',
+        'choices.json',
+    ]
+
+    def test_AAA_change_poll_question(self):
+        poll = Poll.objects.get()
+        poll.question = 'What does the fox say?'
+
+        with set_autocommit(True):
+            with atomic():
+                poll.save()
+
+    def test_BBB_poll_question_is_what_is_in_the_fixture(self):
+        poll = Poll.objects.get()
+        self.assertEqual(poll.question, 'What bear is best?')
+
+
+class NonFixtureBleedThroughTestCase(TestCase):
+    @contextmanager
+    def set_autocommit(self, value):
+        old_value = transaction.get_autocommit()
+        transaction.set_autocommit(value)
+        yield
+        transaction.set_autocommit(old_value)
+
+    def test_AAA_create_a_poll(self):
+        with set_autocommit(True):
+            with atomic():
+                Poll.objects.create(
+                    question='Test',
+                    pub_date=datetime.now(),
+                )
+
+    def test_BBB_no_polls_in_the_db(self):
+        self.assertEqual(Poll.objects.count(), 0)
