eb32dac6b98e9850162cf5073d9254378c59b544
Jason Ward <jason.ward@policystat.com>
2016-01-07 16:11:16 -0500

    refs #44: Fixed the tests.

--- nosedjangotests/polls/tests/test0.py
+++ nosedjangotests/polls/tests/test0.py
@@ -1,12 +1,16 @@
 from contextlib import contextmanager
 from datetime import datetime
 
+import django
 try:
     from django.db.transaction import atomic
 except ImportError:
     from django.db.transaction import commit_on_success as atomic
 from django.db import transaction
+from django.db.transaction import TransactionManagementError
 from django.test import TestCase
+
+from nose.plugins.skip import SkipTest
 
 from nosedjangotests.polls.models import Choice, Poll
 
@@ -41,18 +45,30 @@ def set_autocommit(value):
 
 
 class FixtureBleedThroughTestCase(TestCase):
+    use_transaction_isolation = True
+
     fixtures = [
         'polls1.json',
         'choices.json',
     ]
 
     def test_AAA_change_poll_question(self):
+        if django.VERSION < (1, 7):
+            raise SkipTest('This test is only an issue in django 1.8+')
         poll = Poll.objects.get()
         poll.question = 'What does the fox say?'
 
-        with set_autocommit(True):
-            with atomic():
-                poll.save()
+        # If we are using transaction isolation, then using atomic/autocommit
+        # is invalid.
+        if self.use_transaction_isolation:
+            with self.assertRaises(TransactionManagementError):
+                with set_autocommit(True):
+                    with atomic():
+                        poll.save()
+        else:
+            with set_autocommit(True):
+                with atomic():
+                    poll.save()
 
     def test_BBB_poll_question_is_what_is_in_the_fixture(self):
         poll = Poll.objects.get()
@@ -60,13 +76,28 @@ class FixtureBleedThroughTestCase(TestCase):
 
 
 class NonFixtureBleedThroughTestCase(TestCase):
+    use_transaction_isolation = True
+
     def test_AAA_create_a_poll(self):
-        with set_autocommit(True):
-            with atomic():
-                Poll.objects.create(
-                    question='Test',
-                    pub_date=datetime.now(),
-                )
+        if django.VERSION < (1, 7):
+            raise SkipTest('This test is only an issue in django 1.8+')
+        # If we are using transaction isolation, then using atomic/autocommit
+        # is invalid.
+        if self.use_transaction_isolation:
+            with self.assertRaises(TransactionManagementError):
+                with set_autocommit(True):
+                    with atomic():
+                        Poll.objects.create(
+                            question='Test',
+                            pub_date=datetime.now(),
+                        )
+        else:
+            with set_autocommit(True):
+                with atomic():
+                    Poll.objects.create(
+                        question='Test',
+                        pub_date=datetime.now(),
+                    )
 
     def test_BBB_no_polls_in_the_db(self):
         self.assertEqual(Poll.objects.count(), 0)
