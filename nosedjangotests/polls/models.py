c9e23182d49f9e05f390b8f482c9012d66c69c6d
Bob Cribbs <bob.cribbs@policystat.com>
2016-01-14 19:53:34 +0000

    refs #45 add methods that use atomic as a callable decorator

--- nosedjangotests/polls/models.py
+++ nosedjangotests/polls/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+try:
+    from django.db.transaction import atomic
+except ImportError:  # Django 1.4
+    from django.db.transaction import commit_on_success as atomic
 from django.contrib.contenttypes import generic
 
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    @atomic()
+    def reset(self):
+        self.question = ""
+        self.save()
 
 
 class Choice(models.Model):
@@ -14,3 +23,8 @@ class Choice(models.Model):
 
     choice = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    @atomic()
+    def reset(self):
+        self.votes = 0
+        self.save()
