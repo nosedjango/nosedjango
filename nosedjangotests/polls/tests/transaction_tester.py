6c442791bc2c348c71973876182e76965e8b0570
Bob Cribbs <bob.cribbs@policystat.com>
2016-02-01 17:54:19 +0000

    refs #50 dj14 try except for atomic

--- nosedjangotests/polls/tests/transaction_tester.py
+++ nosedjangotests/polls/tests/transaction_tester.py
@@ -1,4 +1,7 @@
-from django.db.transaction import atomic
+try:
+    from django.db.transaction import atomic
+except ImportError:
+    from django.db.transaction import commit_on_success as atomic
 
 
 @atomic
