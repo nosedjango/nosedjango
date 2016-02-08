dc0a789884c7aa223ad89b403bd95b9746b686be
Kyle Gibson <kyle.gibson@frozenonline.com>
2016-02-08 18:09:10 -0500

    Import the plugin, created a test base class

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 
+from nosedjango.nosedjango import NoseDjango
 
-class CustomURLConfTestCase(TestCase):
+
+class PluginTestBase(TestCase):
+    def setUp(self):
+        super(PluginTestBase, self).setUp()
+        self.plugin = NoseDjango()
+
+
+class CustomURLConfTestCase(PluginTestBase):
     def test_original_url_conf_is_restored_after_test_run(self):
         pass
