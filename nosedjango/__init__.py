9aa4c80a9dafb7c51bdb57c86e6d24fdb1650409
Kyle Gibson <kyle.gibson@frozenonline.com>
2016-02-09 15:48:18 -0500

    Bumped version to 1.0.12

--- nosedjango/__init__.py
+++ nosedjango/__init__.py
@@ -5,7 +5,7 @@ or even write your own plugin that hooks in to the NoseDjango plugin system (a
 plugin with a plugin system!)
 """
 
-VERSION = (1, 0, 11, '')
+VERSION = (1, 0, 12, '')
 
 __version__ = ".".join(map(str, VERSION[:-1]))
 __release__ = ".".join(map(str, VERSION))
